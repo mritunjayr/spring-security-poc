1a3a8999baeada1e02a7f2aaa298283d99969832
mritunjay <mritunjaykumar694@gmail.com>
2019-10-21 04:53:30 +0530

    fix

---
+++ application-login/client-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { LogoutComponent } from "./logout/logout.component";
+import { HeaderComponent } from "./header/header.component";
+import { SuccessComponent } from "./success/success.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    HeaderComponent,
+    SuccessComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}

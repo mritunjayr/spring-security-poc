bb1f4cb8891fac0cca8ab238bcd2b450bf2cfb15
Mritunjay Kumar <mritunjaykumar694@gmail.com>
2024-03-03 22:59:43 +0530

    upgraded to the angular V16

--- basic-authentication/client-app/src/app/service/auth-gaurd.service.ts
+++ basic-authentication/client-app/src/app/service/auth-gaurd.service.ts
@@ -1,16 +1,11 @@
 import { Injectable } from '@angular/core';
-import {
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Router
-} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGaurdService implements CanActivate {
+export class AuthGaurdService  {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (this.authService.isUserLoggedIn()) { return true; }
 
